f2ff2686a7b4b4e11c371c93243849a07b2c6511
MASAO IKAWA <ikawa@axele.co.jp>
2015-06-07 23:53:23 +0900

    Initial Commit

---
+++ Light Switch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light Switch
+//
+//  Created by 井川 雅央 on 2015/06/07.
+//  Copyright (c) 2015年 井川 雅央. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
